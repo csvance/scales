4e7be77de1106253dfec4929d17d8e9eb7b6cda3
Carroll Vance <vancecs@gmail.com>
2017-10-21 13:25:01 -0500

    Fix alignment

--- renderers/PianoRendererText.cpp
+++ renderers/PianoRendererText.cpp
@@ -38,7 +38,7 @@ namespace MusicalScales {
             outputStream << "   |" << std::setw(3) << notes[1] << "||" << std::setw(3) << notes[3] << "|     |"
                          << std::setw(3) << notes[6] << "||" << std::setw(3) << notes[8] << "||" << std::setw(3)
                          << notes[10] << "|   " << std::endl;
-            outputStream << "|    |    |   |   |   |   |   |" << std::endl;
+            outputStream << "|    |    |    |    |    |    |    |" << std::endl;
             outputStream << "|" << std::setw(3) << notes[0] << " |" << std::setw(3) << notes[2] << " |" << std::setw(3)
                          << notes[4] << " |" << std::setw(3) << notes[5] << " |" << std::setw(3) << notes[7] << " |"
                          << std::setw(3) << notes[9] << " |" << std::setw(3) << notes[11] << " |" << std::endl;
